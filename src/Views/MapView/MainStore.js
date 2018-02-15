bd01f8b6a94ba67deaf6265fedfc783cb542d3e9
Nishanth Shankar <nishanths@goodbox.in>
2018-02-15 12:22:27 +0530

    Basic pin structuring done

--- src/Views/MapView/MainStore.js
+++ src/Views/MapView/MainStore.js
@@ -1,4 +1,5 @@
 import {computed, action, observable} from 'mobx'
+import {Keyboard} from 'react-native'
 import _ from 'lodash'
 
 import G from '../../config/globals'
@@ -11,7 +12,8 @@ class MainStore {
     latitude: 12.893800,
     longitude: 77.615558
   }
-
+  @observable selectedMarker = null
+  @observable hiddenId = null
   @observable currentLocations = []
   @computed get topLocations () {
     let locations = this.currentLocations.slice(0, 3)
@@ -35,6 +37,7 @@ class MainStore {
 
   @action.bound
   fetchAreaSuggestions = phrase => dummy => {
+    this.loading = true
     let {latitude, longitude} = this.location
     let url = `https://maps.googleapis.com/maps/api/place/autocomplete/json`
     let fin = `${url}?input=${phrase}&types=(regions)&location=${latitude},${longitude}&key=${G.variables.GOOGLE_KEY}`
@@ -47,44 +50,63 @@ class MainStore {
           label: pred.structured_formatting.main_text
         }))
       })
+      .then(() => { this.loading = false })
   }
 
   @action.bound
   fetchAreaDetails = (placeId, map) => {
     let base = `https://maps.googleapis.com/maps/api/place/details/json`
     let url = `${base}?placeid=${placeId}&key=${G.variables.GOOGLE_KEY}`
+    this.loading = true
     fetch(url)
       .then(d => d.json())
       .then(data => {
         this.currentLocations = []
         let {lat, lng} = data.result.geometry.location
+        Keyboard.dismiss()
         // cb({lat, lng})
         map.animateToCoordinate({latitude: lat, longitude: lng})
-        this.fetchNearbyRestaurants({lat, lng})
+        this.fetchNearbyRestaurants({lat, lng}, map)
         // this.test = 2
       })
+      .then(() => { this.loading = false })
   }
 
   @action.bound
-  fetchNearbyRestaurants = ({lat, lng}) => {
+  fetchNearbyRestaurants = ({lat, lng}, map) => {
     let base = `https://maps.googleapis.com/maps/api/place/nearbysearch/json`
     let url = `${base}?location=${lat},${lng}&radius=1000&type=restaurant&key=${G.variables.GOOGLE_KEY}`
-    console.log('URI:', url)
+    this.loading = true
     fetch(url)
       .then(d => d.json())
       .then(({results}) => {
         this.places = _.map(results, (item) => ({
+          id: item.id,
           location: item.geometry.location,
           name: item.name,
           open: _.get(item, 'opening_hours.open_now', false),
           placeId: item.place_id,
           rating: item.rating
         }))
+        map.fitToElements(true, {edgePadding: {top: 20, right: 10, bottom: 10, left: 10}})
       })
+      .then(() => { this.loading = false })
   }
   @action.bound
   toggleLogin = () => {
-    setTimeout(action(() => { this.loading = true }), 1000)
+    // setTimeout(action(() => { this.loading = true }), 1000)
+  }
+
+  @action.bound
+  selectMarker = (id, i) => {
+    this.hiddenId = id
+    this.selectedMarker = id
+    setTimeout(this.makeMarkerVisible(i), 300)
+  }
+
+  @action.bound
+  makeMarkerVisible = (i) => () => {
+    this.hiddenId = null
   }
 
   @action.bound
--- src/Views/MapView/index.js
+++ src/Views/MapView/index.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
+  Image,
   Text,
   TextInput,
+  ActivityIndicator,
   TouchableOpacity
 } from 'react-native'
 
 import MapView, {Marker} from 'react-native-maps'
 import {observer} from 'mobx-react/native'
+let placeMarker = require('../../assets/place_marker.png')
+let selectedMarkerIcon = require('../../assets/selected_marker.png')
 
 // Styles
 const styles = StyleSheet.create({
@@ -20,7 +24,8 @@ const styles = StyleSheet.create({
     top: 24,
     left: 16,
     right: 16,
-    elevation: 6
+    elevation: 6,
+    paddingHorizontal: 16
   },
   searchResultsHolder: {
     position: 'absolute',
@@ -63,12 +68,15 @@ export default class App extends Component {
     })
   }
   render () {
-    let {location, onChangeText, searchPhrase,
-      topLocations, fetchAreaDetails, topRestaurants} = this.props.store
-    let {latitude, longitude} = location
-    console.log('REST:', topRestaurants)
+    let {location, onChangeText, searchPhrase, selectedMarker, selectMarker,
+      hiddenId, topLocations, fetchAreaDetails, topRestaurants, loading} = this.props.store
+    const {latitude, longitude} = location
+    const True = true
+    console.log("LOADING:", loading)
     return (
-      <View style={[{flex: 1}]}>
+      <View style={StyleSheet.absoluteFill}>
+        <Image source={placeMarker} style={{opacity:0}} />
+        <Image source={selectedMarkerIcon} style={{opacity:0}} />
         <MapView
           ref={mapview => { this.mapview = mapview }}
           style={StyleSheet.absoluteFill}
@@ -79,10 +87,35 @@ export default class App extends Component {
             longitudeDelta: 0.0121
           }}
         >
-          <Marker coordinate={location} />
-          {/* <Marker coordinate={location} >
-            <View style={[{height: 40, width: 40, backgroundColor: 'pink', elevation: 4}]} />
+          {/* <Marker coordinate={location} /> */}
+          {/* <Marker anchor={{x: 0.52, y: 0.5}} coordinate={location} >
+            <Image source={placeMarker} style={{height: 32, width: 32, opacity: 0.6}} />
           </Marker> */}
+          {/* <Marker anchor={{x: 0.525, y: 0.9}} coordinate={location} >
+            <Image source={selectedMarkerIcon} style={{height: 48, width: 32, opacity: 0.6}} />
+          </Marker> */}
+          {
+            topRestaurants.map((rest) => {
+              const {lat: latitude, lng: longitude} = rest.location
+              const size = rest.rating > 3.5 ? 32 : 20
+              let image = rest.id === selectedMarker ? selectedMarkerIcon : placeMarker
+              let height = rest.id === selectedMarker ? size * 1.5 : size
+              let y = rest.id === selectedMarker ? 0.9 : 0.5
+
+              if (rest.id === hiddenId) {
+                return <View key={hiddenId} />
+              }
+              return (
+                <Marker
+                  key={rest.id}
+                  anchor={{x: 0.525, y}}
+                  coordinate={{latitude, longitude}}
+                  onPress={() => selectMarker(rest.id)}>
+                  <Image source={image} style={{height: height, width: size}} />
+                </Marker>
+              )
+            }
+        )}
         </MapView>
         <View style={[styles.searchBar]} >
           <TextInput
@@ -91,8 +124,10 @@ export default class App extends Component {
             value={searchPhrase}
             placeholder='Search location'
             underlineColorAndroid='#0000'
-            style={{marginLeft: 8, marginTop: 4, flex: 1}}
+            style={{marginTop: 2, flex: 1}}
             />
+          {loading ? <ActivityIndicator animating={True} color={'#F1847A'} />
+            : <View />}
         </View>
         <View style={[styles.searchResultsHolder]}>
           {topLocations.map(data => {
